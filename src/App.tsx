eaad0e5d279f9ae764974b234c1fdba81145169a
GiaBruh <baohngce171023@fpt.edu.vn>
2024-01-15 09:50:44 +0700

    Small changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import "./App.css";
 import { StyledButton } from "./components/Presentational/atoms/Button/Button.styles";
 
 function App() {
-  return (
-    <div className="App">
-      <StyledButton>Click</StyledButton>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
