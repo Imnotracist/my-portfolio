51c0a3fe1e968ff6bcee07ec574f35fc560ec30e
GiaBruh <baohngce171023@fpt.edu.vn>
2024-01-15 09:40:29 +0700

    Small changes

---
+++ src/components/Presentational/atoms/Button/Button.styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  background-color: #04aa6d; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+`;
